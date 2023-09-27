627274dc53422b80a46f407b4b453cc98b46fd7c
Pocky <r.dudko3@gmail.com>
2023-09-27 10:21:10 +0300

    E. Подсчёт функции

---
+++ src/main/java/org/kldmns/EПодсчётФункции.java
@@ -0,0 +1,33 @@
+package org.kldmns;
+
+import java.util.Scanner;
+
+/**
+ * Для положительного целого числа n определим функцию f:
+ *
+ * f(n) =  - 1 + 2 - 3 + .. + ( - 1)nn
+ *
+ * Ваша задача — посчитать f(n) для данного целого числа n.
+ *
+ * Входные данные
+ * В единственной строке записано положительное целое число n (1 ≤ n ≤ 1015).
+ *
+ * Выходные данные
+ * Выведите f(n) в единственной строке.
+ */
+public class EПодсчётФункции {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int result;
+
+        if (n % 2 == 0) {
+            result = n / 2;
+        } else {
+            result = -(n / 2 + 1);
+        }
+
+        System.out.println(result);
+    }
+}
