dfa0ca33663de81e364ee963b2b6d2547a975e68
Pocky <r.dudko3@gmail.com>
2023-10-12 17:34:13 +0300

    C. Скучные квартиры

---
+++ src/main/java/org/kldmns/third/C.java
@@ -0,0 +1,25 @@
+package org.kldmns.third;
+
+import java.util.Scanner;
+
+public class C {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            int x = scanner.nextInt();
+            int digit = x % 10;
+            int sum = (digit - 1) * 10;
+
+            int count = 1;
+            while (x > 0) {
+                sum += count;
+                x /= 10;
+                count++;
+            }
+
+            System.out.println(sum);
+        }
+    }
+}
